ef37f3d6da8518304057da9fc69da82829e8d4d3
thuanedesouza <36061136+thuanedesouza@users.noreply.github.com>
2021-08-04 19:47:44 -0300

    ajuste na chamada da url

--- src/app/services/transferencia.service.ts
+++ src/app/services/transferencia.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 })
 export class TransferenciaService {
   private listaTransferencia: any[];
-  private url = 'http://localhost:3000/transferencias'
+  private url = 'http://localhost:3000/Transferencias'
 
   constructor(private httpClient: HttpClient) {//provê metodos de uma api rest
     this.listaTransferencia = [];
@@ -22,9 +22,9 @@ export class TransferenciaService {
   todas(): Observable<Transferencia[]>{//fica escutando novas transferências
     return this.httpClient.get<Transferencia[]>(this.url)
   }
-  adicionar(transferencia: any){
+  adicionar(transferencia: Transferencia): Observable<Transferencia>{
     this.hidratar(transferencia);
-    this.listaTransferencia.push(transferencia);
+    return this.httpClient.post<Transferencia>(this.url, transferencia );
   }
   //por data ser uma regra de negocio do bytebank vamos criar ela aqui
   private hidratar(transferencia: any){//hidratar dados
